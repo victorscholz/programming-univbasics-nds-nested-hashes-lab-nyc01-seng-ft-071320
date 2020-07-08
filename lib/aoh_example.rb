dfa0db57b554656a6722d419c38930cac834c7cb
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-08 05:30:49 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,8 +12,10 @@ end
 
 def literal_aoh
   [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
-    
-    
+  {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}
+  {:name=>"Pat Monteseian", :occupation=>"Staff"}
+  {:name=>"Kate Gompert", :occupation=>"None"}
+  {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
   }]
 end
 
